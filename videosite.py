2916c632e937c9fdaace65e61858cd06b90180d5
kyumaevv <nayeli.derozario@gmail.com>
2025-02-19 17:59:53 +0800

    save

--- videosite.py
+++ videosite.py
@@ -1,7 +1,15 @@
 import streamlit as st
+import webbrowser
+import pandas as pd
+
+try:
+    videocsv=pd.read_csv('videosratings.csv')
+except:
+    videocsv = pd.DataFrame()
 
 st.set_page_config(page_title='Videos site', page_icon='👓')
 menu = st.sidebar.selectbox("Menu",['Videos','Video ratings'])
+
 
 if menu == 'Videos':
     vidcat = st.sidebar.pills('Choose videos',['All','Baking','Music','Education','Animals'],default='All')
@@ -14,22 +22,30 @@ if menu == 'Videos':
         with c1:
             st.image("https://i.ytimg.com/vi/KmLgDi9FMeM/maxresdefault.jpg")
             st.write("Oreo cupcakes recipe")
-            st.link_button(label='Play Video',url='https://www.youtube.com/watch?v=KmLgDi9FMeM&t=46s&pp=ygUNb3JlbyBjdXBjYWtlcw%3D%3D')
+            if st.button('Play Video',key="1"):
+                webbrowser.open("https://www.youtube.com/watch?v=KmLgDi9FMeM&t=46s&pp=ygUNb3JlbyBjdXBjYWtlcw%3D%3D")
+                if "Oreo cupcakes recipe" in videocsv['Videos'].values:
+                    videocsv.loc[videocsv['Videos']=='Oreo cupcakes recipe','Plays'] +=1
+                else:
+                    st.write("no")
 
         with c2:
             st.image("https://i.ytimg.com/vi/KvlAjbfv20M/maxresdefault.jpg")
             st.write("Red velvet cake recipe")
-            st.link_button(label='Play Video',url='https://www.youtube.com/watch?v=KvlAjbfv20M&pp=ygUPcmVkIHZlbHZldCBjYWtl')
-            
+            if st.button('Play video',key="2"):
+                webbrowser.open("https://www.youtube.com/watch?v=KvlAjbfv20M&pp=ygUPcmVkIHZlbHZldCBjYWtl")
+        
         with c3:
             st.image("https://i.ytimg.com/vi/ji8qpeYWpgU/maxresdefault.jpg")
             st.write("Fudgy brownies recipe")
-            st.link_button(label='Play Video',url='https://www.youtube.com/watch?v=l5Z6oT6GAes&pp=ygUOZnVkZ3kgYnJvd25pZXM%3D')
+            if st.button('Play video',key="3"):
+                webbrowser.open("https://www.youtube.com/watch?v=l5Z6oT6GAes&pp=ygUOZnVkZ3kgYnJvd25pZXM%3D")
         
         with c4:
             st.image("https://i.ytimg.com/vi/3orMJXkKk5o/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLAIdlu8Ot-aXlJl9vfxE6-dUQ7KGQ")
             st.write("Sugar cookies recipe")
-            st.link_button(label='Play Video',url='https://www.youtube.com/watch?v=txw2zaUW9UA&pp=ygUUc3VnYXIgY29va2llcyByZWNpcGU%3D')
+            if st.button('Play video',key="4"):
+                webbrowser.open("https://www.youtube.com/watch?v=txw2zaUW9UA&pp=ygUUc3VnYXIgY29va2llcyByZWNpcGU%3D")
     
     if vidcat == 'All' or vidcat == 'Music':
         st.subheader("Music")
@@ -38,19 +54,26 @@ if menu == 'Videos':
         with c1:
             st.image("https://s.abcnews.com/images/Entertainment/240112_abcnl_prime_uchis1_hpMain_16x9_1600.jpg")
             st.write("Dead to me")
-            st.link_button(label='Play video',url='https://www.youtube.com/watch?v=OcUDK4kAUIw&pp=ygUKZGVhZCB0byBtZQ%3D%3D')
+            if st.button('Play vide',key="5"):
+                webbrowser.open("https://www.youtube.com/watch?v=OcUDK4kAUIw&pp=ygUKZGVhZCB0byBtZQ%3D%3D")
+    
         with c2:
             st.image("https://i.ytimg.com/vi/o7fgFaXKVa0/maxresdefault.jpg")
             st.write("Duvet")
-            st.link_button(label='Play video',url='https://www.youtube.com/watch?v=o7fgFaXKVa0&ab_channel=B%C3%B4a-Topic')
+            if st.button('Play video',key="6"):
+                webbrowser.open("https://www.youtube.com/watch?v=o7fgFaXKVa0&ab_channel=B%C3%B4a-Topic")
+    
         with c3:
             st.image("https://i.ytimg.com/vi/laDnsiKURTQ/maxresdefault.jpg")
             st.write("Apple cider")
-            st.link_button(label='Play video',url='https://www.youtube.com/watch?v=laDnsiKURTQ&ab_channel=Beabadoobee')
+            if st.button('Play video',key="7"):
+                webbrowser.open("https://www.youtube.com/watch?v=laDnsiKURTQ&ab_channel=Beabadoobee")
+
         with c4:
             st.image("https://i.ytimg.com/vi/6ztwSTu5B8A/maxresdefault.jpg")
             st.write("Mosquito")
-            st.link_button(label='Play video',url='https://www.youtube.com/watch?v=6ztwSTu5B8A')
+            if st.button('Play video',key="8"):
+                webbrowser.open("https://www.youtube.com/watch?v=6ztwSTu5B8A")
 
     if vidcat == 'All' or vidcat == 'Education':
 
@@ -60,22 +83,26 @@ if menu == 'Videos':
         with c1:
             st.image("https://i.ytimg.com/vi/3tisOnOkwzo/maxresdefault.jpg")
             st.write("Biology explained")
-            st.link_button(label='Play video',url='https://www.youtube.com/watch?v=3tisOnOkwzo&pp=ygURYmlvbG9neSBleHBsYWluZWQ%3D')
+            if st.button("Play video",key="9"):
+                webbrowser.open("https://www.youtube.com/watch?v=3tisOnOkwzo&pp=ygURYmlvbG9neSBleHBsYWluZWQ%3D")
         
         with c2:
             st.image("https://i.ytimg.com/vi/xuCn8ux2gbs/maxresdefault.jpg")
             st.write("History of the world")
-            st.link_button(label='Play video',url='https://www.youtube.com/watch?v=xuCn8ux2gbs&pp=ygUbaGlzdG9yeSBvZiB0aGUgZW50aXJlIHdvcmxk')
+            if st.button("Play video",key="10"):
+                webbrowser.open("https://www.youtube.com/watch?v=xuCn8ux2gbs&pp=ygUbaGlzdG9yeSBvZiB0aGUgZW50aXJlIHdvcmxk")
 
         with c3:
             st.image("https://i.ytimg.com/vi/3kE_Q-JdlO4/maxresdefault.jpg")
             st.write("Factorising quadratics")
-            st.link_button(label='Play video',url='https://www.youtube.com/watch?v=3kE_Q-JdlO4&pp=ygUWZmFjdG9yaXNpbmcgcXVhZHJhdGljcw%3D%3D')
+            if st.button("Play video",key="11"):
+                webbrowser.open("https://www.youtube.com/watch?v=3kE_Q-JdlO4&pp=ygUWZmFjdG9yaXNpbmcgcXVhZHJhdGljcw%3D%3D")
         
         with c4:
             st.image("https://i.ytimg.com/vi/OtBqDgOsW1c/maxresdefault.jpg")
             st.write("Music theory")
-            st.link_button(label='Play video',url='https://www.youtube.com/watch?v=OtBqDgOsW1c&ab_channel=NoMad')
+            if st.button("Play video",key="12"):
+                webbrowser.open("https://www.youtube.com/watch?v=OtBqDgOsW1c&ab_channel=NoMad")
     
     if vidcat == 'All' or vidcat == 'Animals':
 
@@ -85,19 +112,23 @@ if menu == 'Videos':
         with c1:
             st.image("https://i.ytimg.com/vi/lArqN_zu5tw/maxresdefault.jpg")
             st.write("Tortoise eating fruits")
-            st.link_button(label='Play video',url='https://www.youtube.com/watch?v=lArqN_zu5tw&pp=ygUUdHVydGxlIGVhdGluZyBncmFwZXM%3D')
+            if st.button("Play video",key="13"):
+                webbrowser.open("https://www.youtube.com/watch?v=lArqN_zu5tw&pp=ygUUdHVydGxlIGVhdGluZyBncmFwZXM%3D")
 
         with c2:
             st.image("https://i.ytimg.com/vi/Zb3Wzs2FcFE/maxresdefault.jpg")
             st.write("Dog care guide")
-            st.link_button(label='Play video',url='')
+            if st.button("Play video",key="14"):
+                webbrowser.open("https://www.youtube.com/watch?v=peUVLEUj-AM&pp=ygUOZG9nIGNhcmUgZ3VpZGU%3D")
         
         with c3:
             st.image("https://i.ytimg.com/vi/xZS2kMKtBDs/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLAubOlR89v5a2T2oLsN8VQSf7_0Gg")
             st.write("Cat cucumber prank")
-            st.link_button(label='Play video',url='https://www.youtube.com/watch?v=RBrZsgy4-SQ&pp=ygUMY2F0IGN1Y3VtYmVy')
+            if st.button("Play video",key="15"):
+                webbrowser.open("https://www.youtube.com/watch?v=RBrZsgy4-SQ&pp=ygUMY2F0IGN1Y3VtYmVy")
 
         with c4: 
             st.image("https://i.ytimg.com/vi/KYnK5OuFdCE/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLCq3k8D0s5BPS9eXgTHPsjC9n2MJg")
             st.write("Betta fish ecosystem")
-            st.link_button(label='Play video',url='https://www.youtube.com/watch?v=KYnK5OuFdCE&pp=ygURYXF1YXJpdW0gYnVpbGRpbmc%3D')
+            if st.button("Play video",key="16"):
+                webbrowser.open("https://www.youtube.com/watch?v=RBrZsgy4-SQ&pp=ygUMY2F0IGN1Y3VtYmVy")
