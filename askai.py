0e701b8ef127bac8e47ccdb272e8d6001f080c45
kyumaevv <nayeli.derozario@gmail.com>
2026-04-29 17:44:28 +0800

    save

--- askai.py
+++ askai.py
@@ -1,6 +1,7 @@
 import streamlit as st
+import requests
 
-apikey = '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'
+apikey = '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'
 apilink = "https://openrouter.ai/api/v1/chat/completions" #THIS CONNECTS TO OPENROUTER
 headers = {'Authorization': f'Bearer {apikey}', 'Content-Type': 'application/json'}
 
@@ -17,28 +18,28 @@ def askai(content):
     else:
         return f"Error: {response.status_code}"
 
-topic = st.selectbox('choose a python topic',['variables','lists','functions','dictionaries'])
+topic = st.sidebar.selectbox('choose a python topic',['variables','lists','functions','dictionaries','while loops','for loops','if else','range'])
 
 explanation = f"""give a breif, concise summary on the python topic {topic} with examples while still keeping it detailed and informative
 """
 
 question = f"""make a multiple choice question on python {topic} without giving the answer"""
 
-generate = st.pills('',['Generate topic'])
+generate = st.sidebar.pills('',['Generate topic'])
 if generate:
-    with st.spinner('Generating..')
-    expinfo = askai(explanation)
-    qsinfo = askai(question)
-    st.subheader(f'about python {topic}:')
-    st.success(expinfo)
+    with st.spinner('Generating..'):
+        expinfo = askai(explanation)
+        qsinfo = askai(question)
+        st.subheader(f'About python {topic}:')
+        st.info(expinfo)
 
-    answer = f"""answer this question: {qsinfo}"""
-    ansinfo = askai(answer)
+        answer = f"""answer this question: {qsinfo}"""
+        ansinfo = askai(answer)
 
-    tab1, tab2, st.tabs{['question','answer']}
-    with tab1:
-        st.subheader("Question on the topic")
-        st.info(qsinfo)
-    with tab2:
-        st.subheader("Answer")
-        st.info(ansinfo)
+        tab1, tab2 = st.tabs(['question','answer'])
+        with tab1:
+            st.subheader("Question on the topic")
+            st.info(qsinfo)
+        with tab2:
+            st.subheader("Answer")
+            st.success(ansinfo)
